5b6d6935626bddb08caaa0479a61fca0afea1e11
tuyishimejeanpierre-byte <tuyishime.jeanpierre@student.moringaschool.com>
2026-06-15 13:25:58 +0300

    added member and admin

---
+++ models/admin.py
@@ -0,0 +1,9 @@
+class Admin:
+    def __init__(self,user_id,username,password_harsh):
+        super().__init__(
+            user_id = user_id,
+            username = username,
+            password_harsh = password_harsh,
+            role = "admin"
+
+        )
